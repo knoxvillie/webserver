f19d429d33b64ebe9261b464c865584ee93448ea
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2024-02-07 12:46:03 +0000

    changed error message

--- WebServer/srcs/main.cpp
+++ WebServer/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 09:36:33 by kfaustin          #+#    #+#             */
-/*   Updated: 2024/02/07 12:37:21 by diogmart         ###   ########.fr       */
+/*   Updated: 2024/02/07 12:45:49 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 #include "Parser.hpp"
 
 int	main(int argc, char* argv[]) {
+	
 	if (argc > 2) {
-		std::cout << "Webserver must have only 1 argument" << std::endl;
-		return (1);
-	}
+        MERROR("wrong number of arguments.\nTry: \"./webserv [configuration file]\"");
+    }
+	
 	const std::string config_file(argc == 1 ? "../conf/default.conf" : argv[1]);
 	MLOG("Config file path", config_file);
+	
 	try {
 		Parser::parsingConfigFile(config_file);
-	} catch (const std::runtime_error& except) {
+	}
+	catch (const std::runtime_error& except) {
 		std::cerr << except.what() << std::endl;
 	}
+	
 	return (0);
 }
