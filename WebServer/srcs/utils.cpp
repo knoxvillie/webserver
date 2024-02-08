ba9ed974aa2a5ce445e90e78e1e944474653d0a2
Kelvin Faustino <kelvinfaustino@gmail.com>
2024-02-08 17:27:21 +0000

    location block, compiling not tested

--- WebServer/srcs/utils.cpp
+++ WebServer/srcs/utils.cpp
@@ -6,12 +6,11 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 12:51:54 by kfaustin          #+#    #+#             */
-/*   Updated: 2024/02/12 17:15:12 by kfaustin         ###   ########.fr       */
+/*   Updated: 2024/02/08 17:10:57 by kfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
-#include "Server.hpp"
 
 std::vector<std::string>
 vectorInitializer(const char** list) {
@@ -21,29 +20,38 @@ vectorInitializer(const char** list) {
 	return (result);
 }
 
+bool
+isTokenInDirectives(const std::string& token, const std::string& block) {
+	const char* directives[] = {"listen", "server_name", "host", "root", "index",
+								"charset", "access_log","error_log", "error_page",
+								"location", "client_max_body_size" , NULL};
+	if (block == "location") {
+		const char* directives[] = {"autoindex", "allow_methods", "cgi_pass"};
+		(void)directives;
+	}
+	for (int i = 0; directives[i]; i++) {
+		if (token == directives[i])
+			return (true);
+	}
+	return (false);
+
+//	std::vector<std::string> server_directives(vectorInitializer(directives));
+//	for (size_t i = 0; i < server_directives.size(); i++) {
+//		if (token == server_directives[i])
+//			return (true);
+//	}
+}
+
 std::vector<std::string>
-extractValues(const std::string& input) {
+splitString (const std::string& input) {
 	std::vector<std::string> result;
 	std::stringstream ss(input);
 	std::string token;
 
-	ss >> token; //skipping the (directive) the first element of input.
+	ss >> token;
 	for (;ss >> token;)
 			result.push_back(token);
 	return (result);
 }
-
-void
-printServer(std::vector<Server>& serverList) {
-	for (size_t i = 0; i < serverList.size(); i++) {
-		std::cout << "Server " << i << " :" << std::endl;
-		printMapVec(serverList[i].getServerDirectives());
-		if (serverList[i].getLocationDirectives().empty())
-			std::cout << "None Location block" << std::endl;
-		else
-			printMapMapVec(serverList[i].getLocationDirectives());
-	}
-}
-
 
 
