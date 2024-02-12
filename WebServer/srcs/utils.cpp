845b5a01286814d5d4bea6ed2c77c6a88f8105a2
Kelvin Faustino <kelvinfaustino@gmail.com>
2024-02-12 13:28:37 +0000

    parser keep going

--- WebServer/srcs/utils.cpp
+++ WebServer/srcs/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 12:51:54 by kfaustin          #+#    #+#             */
-/*   Updated: 2024/02/08 17:10:57 by kfaustin         ###   ########.fr       */
+/*   Updated: 2024/02/12 11:52:26 by kfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,12 @@ vectorInitializer(const char** list) {
 
 bool
 isTokenInDirectives(const std::string& token, const std::string& block) {
-	const char* directives[] = {"listen", "server_name", "host", "root", "index",
+	const char* server_directives[] = {"listen", "server_name", "host", "root", "index",
 								"charset", "access_log","error_log", "error_page",
 								"location", "client_max_body_size" , NULL};
-	if (block == "location") {
-		const char* directives[] = {"autoindex", "allow_methods", "cgi_pass"};
-		(void)directives;
-	}
+	const char* location_directives[] = {"autoindex", "allow_methods", "cgi_pass", NULL};
+	const char** directives = (block == "location" ? location_directives : server_directives);
+
 	for (int i = 0; directives[i]; i++) {
 		if (token == directives[i])
 			return (true);
@@ -43,15 +42,16 @@ isTokenInDirectives(const std::string& token, const std::string& block) {
 }
 
 std::vector<std::string>
-splitString (const std::string& input) {
+extractValues(const std::string& input) {
 	std::vector<std::string> result;
 	std::stringstream ss(input);
 	std::string token;
 
-	ss >> token;
+	ss >> token; //skipping the (directive) the first element of input.
 	for (;ss >> token;)
 			result.push_back(token);
 	return (result);
 }
+
 
 
